1c86a22cff4923d6a2d3c374d6d217b575a15b32
Brett Miller <miller.brett.andrew@gmail.com>
2017-07-16 22:03:32 -0500

    changed localhost

--- modules/user.js
+++ modules/user.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('localhost:27017/forageFinder');
+mongoose.connect('mongodb://heroku_bsck088f:s6lo3oobfgqnic05lf17vgacvc@ds157268.mlab.com:57268/heroku_bsck088f');
 
 var userSchema = new mongoose.Schema({
   username: String,
