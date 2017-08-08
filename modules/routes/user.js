c8575fba819a259254fb1b1b0a0e84400efe7654
Brett Miller <miller.brett.andrew@gmail.com>
2017-08-08 15:02:48 -0500

    .

---
+++ modules/routes/user.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var express = require('express');
+var router = express.Router();
+var bcrypt = require('bcrypt');
+var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://heroku_bsck088f:s6lo3oobfgqnic05lf17vgacvc@ds157268.mlab.com:57268/heroku_bsck088f');
+// localhost:27017/forageFinder
+
+
+var userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+var userModel = mongoose.model('userModel', userSchema);
+
+module.exports = router;
